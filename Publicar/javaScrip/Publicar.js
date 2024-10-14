5f49d36fe82ff56b2872930adbc75556b5c862ac
Neison1301 <neisonssj@gmail.com>
2024-10-14 14:55:43 -0500

    Avance2.1.1

---
+++ Publicar/javaScrip/Publicar.js
@@ -0,0 +1,5 @@
+window.addEventListener("load",function(){
+    document.getElementById("btenviar").addEventListener("click",function(){
+      alert('GRACIAS POR ENVIAR TU INFORMACION CAUSA')
+    })
+  })
